d8a2a202ccd68df507f66e28880bf8ee749b8e79
bbrunoo <brunoedsanntos@gmail.com>
2022-05-29 13:37:13 -0300

    atv08

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+int valorA = 0;
+int valorB = 0;
+int result = 0;
+
+WriteLine("Escreva o valor A");
+valorA = Convert.ToInt32(ReadLine());
+
+WriteLine("Escreva o valor B");
+valorB = Convert.ToInt32(ReadLine());
+
+if (valorA < 0)
+{
+    WriteLine("Valor Negativo!");
+
+    result = valorA - valorB;
+    WriteLine($"Resultado é: {result}");
+
+}
+
+else
+{
+    WriteLine("Valor Positvo");
+
+    result = valorA - valorB;
+    WriteLine($"Resultado é: {result}");
+}
+
